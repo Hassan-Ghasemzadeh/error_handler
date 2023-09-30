389bf504de270d72e21025ce18ec0bd60bc25fc8
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2023-09-30 17:30:40 +0330

    [chore] : error handler use_case

---
+++ lib/lib/domain/use_case/future_async.dart
@@ -0,0 +1,15 @@
+import 'package:error_handler/lib/data/entity/future_response.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../core/utils/use_case.dart';
+import '../repository/local_error_handler_repository.dart';
+
+class FutureAsyncUseCase<T> extends UseCase<FutureResponse<T>, T Function()> {
+  LocalErrorHandlerRepositoryImplementation get repository =>
+      GetIt.I.get<LocalErrorHandlerRepositoryImplementation>();
+
+  @override
+  Future<FutureResponse<T>> invoke(T Function() param) {
+    return repository.futureAsync(param);
+  }
+}
