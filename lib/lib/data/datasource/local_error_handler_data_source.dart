222769bc8738a8b82fde3c4e62c9a4c55f63b81f
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2023-09-30 16:55:34 +0330

    [fix] : fixed minor bug in datasource

--- lib/lib/data/datasource/local_error_handler_data_source.dart
+++ lib/lib/data/datasource/local_error_handler_data_source.dart
@@ -2,11 +2,11 @@ import 'package:error_handler/lib/data/entity/error_catcher.dart';
 import 'package:error_handler/lib/data/entity/future_response.dart';
 
 class LocalErrorHandlerDataSource {
-  Future<FutureResponse<T>> futureAsync<T>(Function<T>() action) async {
+  Future<FutureResponse<T>> futureAsync<T>(T Function() action) async {
     T? data;
     ErrorCatcher? catcher;
     try {
-      data = action<T>();
+      data = action();
     } catch (e, stackTrace) {
       catcher = ErrorCatcher(message: e.toString(), stackTrace: stackTrace);
     }
