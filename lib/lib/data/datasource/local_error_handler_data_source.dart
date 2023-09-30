0b848df07db2711cdb3e48c079c09ee5b0b4d442
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2023-09-30 17:06:55 +0330

    [docs] : renamed a field

--- lib/lib/data/datasource/local_error_handler_data_source.dart
+++ lib/lib/data/datasource/local_error_handler_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:error_handler/lib/data/entity/error_catcher.dart';
 import 'package:error_handler/lib/data/entity/future_response.dart';
+import 'package:flutter/foundation.dart';
 
 class LocalErrorHandlerDataSource {
   Future<FutureResponse<T>> futureAsync<T>(T Function() action) async {
@@ -8,8 +9,11 @@ class LocalErrorHandlerDataSource {
     try {
       data = action();
     } catch (e, stackTrace) {
+      if (kDebugMode) {
+        print("Error: ${e.toString()}, Stacktrace: $stackTrace");
+      }
       catcher = ErrorCatcher(message: e.toString(), stackTrace: stackTrace);
     }
-    return FutureResponse(data: data, error: catcher);
+    return FutureResponse(data: data, errorCatcher: catcher);
   }
 }
