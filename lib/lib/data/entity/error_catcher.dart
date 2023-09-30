a4230e3c906bc0a964e12b35f0d5cbda37f9f2e2
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2023-09-30 16:01:36 +0330

    [Chore] : entity for error

---
+++ lib/lib/data/entity/error_catcher.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class ErrorCatcher extends Equatable {
+  final String message;
+  final StackTrace stackTrace;
+
+  const ErrorCatcher({
+    required this.message,
+    required this.stackTrace,
+  });
+
+  @override
+  List<Object?> get props => [message, stackTrace];
+}
