2f8bb2b6d893806b632fd92cd8b7d75218e19f2c
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2023-09-30 16:51:05 +0330

    [chore] : function to handle error added

--- lib/lib/data/datasource/local_error_handler_data_source.dart
+++ lib/lib/data/datasource/local_error_handler_data_source.dart
@@ -1,7 +1,15 @@
+import 'package:error_handler/lib/data/entity/error_catcher.dart';
 import 'package:error_handler/lib/data/entity/future_response.dart';
 
-class LocalErrorHandlerDataSource<T> {
-  Future<FutureResponse<T>> futureAsync() async {
-    throw UnimplementedError();
+class LocalErrorHandlerDataSource {
+  Future<FutureResponse<T>> futureAsync<T>(Function<T>() action) async {
+    T? data;
+    ErrorCatcher? catcher;
+    try {
+      data = action<T>();
+    } catch (e, stackTrace) {
+      catcher = ErrorCatcher(message: e.toString(), stackTrace: stackTrace);
+    }
+    return FutureResponse(data: data, error: catcher);
   }
 }
