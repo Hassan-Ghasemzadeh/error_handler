714ac3d5cdbf2a2b078f81942208ae95174825cb
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2023-10-01 10:31:17 +0330

    [feat] : ability to handle app error added

--- lib/error_handler.dart
+++ lib/error_handler.dart
@@ -2,6 +2,7 @@ library error_handler;
 
 import 'package:error_handler/core/get_it_config/get_it_config.dart';
 import 'package:error_handler/lib/domain/use_case/future_async.dart';
+import 'package:error_handler/lib/domain/use_case/register_error_handler.dart';
 
 import 'lib/data/entity/future_response.dart';
 
@@ -13,5 +14,10 @@ class ErrorHandler {
   Future<FutureResponse<T>> futureAsync<T>(T Function() action) async {
     final future = FutureAsyncUseCase<T>();
     return future.invoke(action);
+  }
+
+  Future<void> registerErrorHandler() async {
+    final register = RegisterErrorHandlerUseCase();
+    return register.invoke();
   }
 }
