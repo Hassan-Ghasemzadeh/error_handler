242dbe5c9995ce4203465ddc5e2c977cf04542fd
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2023-10-01 10:30:52 +0330

    [chore] : register remote error handler di

--- lib/core/get_it_config/get_it_config.dart
+++ lib/core/get_it_config/get_it_config.dart
@@ -1,14 +1,23 @@
 import 'package:error_handler/lib/data/datasource/local_error_handler_data_source.dart';
+import 'package:error_handler/lib/data/datasource/remote_error_handler_data_source.dart';
 import 'package:error_handler/lib/domain/repository/local_error_handler_repository.dart';
+import 'package:error_handler/lib/domain/repository/remote_error_handler_repository.dart';
 import 'package:get_it/get_it.dart';
 
 class GetItConfiguration {
   static final GetIt _injector = GetIt.I;
 
   static Future<void> init() async {
+    // register local error handler --------------------------------------------
     _injector.registerSingleton(
       LocalErrorHandlerRepositoryImplementation(
         source: LocalErrorHandlerDataSource(),
+      ),
+    );
+    //  register local error handler -------------------------------------------
+    _injector.registerSingleton(
+      RemoteErrorHandlerRepositoryImplementation(
+        source: RemoteErrorHandlerDataSource(),
       ),
     );
   }
