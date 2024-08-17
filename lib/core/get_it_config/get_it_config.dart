b44dcea8be5a4817820174f343b1a0071137d2c9
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2024-08-17 15:08:28 +0330

    [Feat] : debug print is changed with logger

--- lib/core/get_it_config/get_it_config.dart
+++ lib/core/get_it_config/get_it_config.dart
@@ -1,6 +1,7 @@
 import 'package:error_handler/lib/data/datasource/error_handler_data_source.dart';
 import 'package:error_handler/lib/domain/repository/error_handler_repository.dart';
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 
 class GetItConfiguration {
   static final GetIt _injector = GetIt.I;
@@ -12,5 +13,7 @@ class GetItConfiguration {
         source: ErrorHandlerDataSource(),
       ),
     );
+    //-----------------------------------------------------------------------
+    Logger().w("ERROR HANDLER INITIALIZATION IS COMPLETED.ENJOY USING IT");
   }
 }
--- lib/lib/data/datasource/error_handler_data_source.dart
+++ lib/lib/data/datasource/error_handler_data_source.dart
@@ -27,6 +27,7 @@ class ErrorHandlerDataSource {
 
   //TODO Errors should be sent to server but for now it's ok to just print it
   Future<void> registerErrorHandler() async {
+    Logger().w("REGISTERING ERROR HANDLER HAS BEING STARTED");
     ErrorResponse? errorResponse;
     FlutterError.onError = (details) {
       errorResponse = ErrorResponse(
