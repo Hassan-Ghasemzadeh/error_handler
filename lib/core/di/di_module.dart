2b4066a6f25113941dffc4803a957ac0a1927de5
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-06-30 08:04:11 +0330

    [Feat] : we put comment everywhere needed in **di_module**

--- lib/core/di/di_module.dart
+++ lib/core/di/di_module.dart
@@ -1,5 +1,16 @@
 import 'package:get_it/get_it.dart';
 
+/// An abstract base class that defines a contract for dependency injection modules.
+///
+/// Classes implementing or extending [DIModule] are responsible for organizing
+/// and registering specific sets of dependencies (e.g., services, repositories, BLoCs)
+/// into the service locator.
 abstract class DIModule {
+  /// Registers dependencies into the provided [GetIt] container.
+  ///
+  /// Subclasses must override this method to define their specific
+  /// dependency registration logic.
+  ///
+  /// [injector] The instance of the [GetIt] service locator used for registration.
   void register(GetIt injector);
 }
