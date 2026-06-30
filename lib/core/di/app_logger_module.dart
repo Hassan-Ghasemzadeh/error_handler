57e942651ec9885e2f299893e7e67304ef95009f
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-06-30 07:57:58 +0330

    [Fix] : fixed import in app_logger_module.dart

--- lib/core/di/app_logger_module.dart
+++ lib/core/di/app_logger_module.dart
@@ -1,6 +1,6 @@
  import 'package:get_it/get_it.dart';
 
-import '../../lib/src/logging/app_logger.dart';
+import 'package:resultex/lib/src/logging/app_logger.dart';
 import 'di_module.dart';
 
 class AppLoggerModule extends DIModule {
