5522f39656abc60ef942d5832546f979fe6a5e47
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-07-01 10:50:10 +0330

    [Feat] :  just has been formated

--- packages/resultex_logger/lib/resultex_logger.dart
+++ packages/resultex_logger/lib/resultex_logger.dart
@@ -1,6 +1,6 @@
 /// The main entry point of the `resultex_logger` package.
 ///
-/// This library exports the core logging functionalities, allowing consuming 
+/// This library exports the core logging functionalities, allowing consuming
 /// applications to import a single file to access the complete logging system.
 library;
 
