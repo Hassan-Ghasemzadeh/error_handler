b38c60c4fe557c4123a43848d2b7f3f8ce77c0bc
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-07-01 10:46:30 +0330

    [Feat] : get it config completed

---
+++ packages/resultex_logger/lib/core/di/resultex_logger_module.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+import '../../src/logger.dart';
+import 'di_module.dart';
+
+/// A dependency injection module responsible for registering the [AppLogger].
+///
+/// This module extends [DIModule] to integrate with the application's
+/// dependency injection lifecycle.
+class ResultexLoggerModule extends DIModule {
+  /// Registers the dependencies managed by this module into the [GetIt] injector.
+  ///
+  /// This method is called during the application initialization phase.
+  @override
+  void register(GetIt injector) {
+    // Register AppLogger as a lazy singleton.
+    // It will only be instantiated when it is requested for the first time.
+    injector.registerLazySingleton<ResultexLogger>(() => ResultexLogger());
+  }
+}
