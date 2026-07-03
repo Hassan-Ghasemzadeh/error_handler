3e5ff06cdd7c6a240cad4bf0beba699f62cf0c8f
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-07-03 09:57:07 +0330

    [Feat] : Configuration options for configuring exponential or linear retry behavior completed

---
+++ packages/resultex/lib/src/model/retry_options.dart
@@ -0,0 +1,18 @@
+/// Configuration options for configuring exponential or linear retry behavior.
+class RetryOptions {
+  /// The maximum number of retry attempts before giving up.
+  final int maxAttempts;
+
+  /// The initial delay duration before the first retry attempt.
+  final Duration delay;
+
+  /// An optional factor to multiply the delay by on each subsequent attempt (Exponential Backoff).
+  /// Defaults to 1.0 (Linear delay).
+  final double backoffFactor;
+
+  const RetryOptions({
+    this.maxAttempts = 3,
+    this.delay = const Duration(seconds: 2),
+    this.backoffFactor = 1.0,
+  });
+}
