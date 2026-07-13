080b5be8043c4a63a96f4bacc305d798c33a5cc2
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-07-13 17:45:39 +0330

    [Feat]: used matcher features in this test file

---
+++ packages/resultex/test/matcher_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:resultex/resultex.dart';
+import 'package:resultex/src/model/failure.dart';
+import 'package:resultex/src/resultex_test.dart'; // مچرهای خودت
+
+void main() {
+  group('Resultex Custom Matchers Verification', () {
+    test('isSuccess should validate SuccessResult correctly', () {
+      final Result<String> sampleSuccess = Result.success('Hello Ara');
+
+      expect(sampleSuccess, isSuccess<String>());
+      expect(sampleSuccess, isSuccess<String>('Hello Ara'));
+    });
+
+    test('isFailure should validate FailureResult correctly', () {
+      final Result<String> sampleFailure = Result.failure(Failure(message: 'Database crashed'));
+
+      expect(sampleFailure, isFailure());
+      expect(sampleFailure, isFailure('Database crashed'));
+    });
+  });
+}
